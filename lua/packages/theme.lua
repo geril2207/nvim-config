c33404e8342ce2c130634ba5e35269731270d74a
Gergert Ilya <gergert.ilya@mail.ru>
2022-09-29 23:34:51 +0300

    change theme to tokyonight

--- lua/packages/theme.lua
+++ lua/packages/theme.lua
@@ -107,7 +107,29 @@ require('onedark').setup  {
     },
 }
 
+
+require("tokyonight").setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  transparent = true, -- Enable this to disable setting the background color
+  terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    comments = { italic = false},
+    keywords = { italic = false},
+    functions = { italic = false},
+    variables = { italic = false},
+    sidebars = "transparent", -- style for sidebars, see below
+    floats = "transparent", -- style for floating windows
+  },
+  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.4, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+})
+
 require("bufferline").setup{}
 
-vim.cmd [[colorscheme catppuccin]]
+vim.cmd [[colorscheme tokyonight]]
 vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
