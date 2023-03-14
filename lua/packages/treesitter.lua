1b2198090806571100417f752309924b504fc0aa
Gergert Ilya <gergert.ilya@mail.ru>
2023-03-14 15:32:05 +0300

    feat(tresitter): update ensure installed

--- lua/packages/treesitter.lua
+++ lua/packages/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-	ensure_installed = { "javascript", "lua", "tsx", "typescript", "scss", "css", "json", "dot", "html", "prisma" },
+	ensure_installed = { "javascript", "lua", "tsx", "typescript", "scss", "css", "json", "dot", "html", "prisma" , "markdown"},
 	sync_install = false,
 
 	highlight = {
