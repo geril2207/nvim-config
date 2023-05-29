3338e059987f590d30bb46992c82e237c36af786
Gergert Ilya <gergert.ilya@mail.ru>
2023-05-29 10:34:05 +0300

    feat: add auto inline diagnostics

--- lua/settings.lua
+++ lua/settings.lua
@@ -61,3 +61,10 @@ augroup end
 
 -- SET HTML AS HTML NOT DJANGO
 cmd [[au BufNewFile,BufRead *.html set filetype=html]]
+
+
+-- Lsp diagnostic auto
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 250
+cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
