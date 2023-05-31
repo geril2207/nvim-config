d57ebf1d22909142009fdbff17f7a73a3ad98449
Gergert Ilya <gergert.ilya@mail.ru>
2023-05-31 09:40:01 +0300

    feat: update neovide font

--- init.lua
+++ init.lua
@@ -6,4 +6,4 @@ require('keymaps')
 -- vim.g.neovide_fullscreen = true
 vim.g.neovide_cursor_animate_in_insert_mode = true
 vim.g.neovide_cursor_antialiasing = true
-vim.o.guifont = "JetBrainsMono Nerd Font:h11"
+vim.o.guifont = "JetBrainsMono Nerd Font:h13"
