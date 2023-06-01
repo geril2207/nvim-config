e39aeac3fc9f3b0a24009e4a921f686d48fc7907
Gergert Ilya <gergert.ilya@mail.ru>
2023-06-01 11:46:19 +0300

    fix: remove unused keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -29,35 +29,21 @@ map("v", "K", "<ESC>", {})
 map("n", "<C-z>", ":undo<CR>", {})
 map("n", "<C-Z>", ":redo<CR>", {})
 map("n", "<C-s>", ":w<CR>", {})
-map("n", "<C-w>", ":Bdelete<CR>", {})
 map("v", "Y", '"+y', {})
 map("v", "<C-c>", '"+y', {})
 map("n", "Y", '"+y', {})
 map("n", "<leader>qq", ":q!<CR>:q!<CR>:q!<CR>:q!<CR>", {})
--- BufferLine
--- map('n', 'J', ':BufferLineCyclePrev<CR>', {})
 map("n", "<C-u>", "<C-u>zz")
 map("n", "<C-d>", "<C-d>zz")
 
--- map('n', '<Tab>', ':BufferLineCycleNext<CR>', {})
 map("n", "<Tab>", ':lua require("harpoon.ui").nav_next()<CR>', {})
 map("n", "<S-Tab>", ':lua require("harpoon.ui").nav_prev()<CR>', {})
--- map('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', {})
--- map('n', 'K', ':BufferLineCycleNext<CR>', {})
--- map("n", "<leader>kw", ":BufferCloseAllButCurrentOrPinned<CR>", {})
--- Example maps
--- map('n', '<A-1>', ':BufferLineGoToBuffer 1<CR>', {})
--- map('n', '<A-1>', ':lua require("harpoon.ui").nav_file(1)<CR>', {})
 
--- Map 1-9 navigation harpoon
 for i = 1, 9, 1 do
 	map("n", "<A-" .. i .. ">", ':lua require("harpoon.ui").nav_file(' .. i .. ")<CR>")
 end
--- -- Map 1-9 navigation bufferline
--- for i = 1,9,1
--- do
--- map('n', '<A-' .. i .. '>', ':BufferLineGoToBuffer ' ..i .. ')<CR>')
--- end
+map("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>")
+map("n", "<leader>hl", ":lua require('harpoon.ui').toggle_quick_menu()<CR>")
 
 --Telescope
 map("n", "<leader>ff", ":Telescope find_files<CR>", {})
@@ -81,7 +67,6 @@ map("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", {})
 map("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", {})
 map("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", {})
 map("i", "<C-u>", "<C-g>u<C-u>", { silent = true })
--- map("n", "<A-F>", ":Prettier<CR>", {})
 
 -- Maps For Double Leader
 -- Easy Motions
@@ -90,7 +75,3 @@ mapDoubleLeader("n", "b", ":HopWordBC<CR>", {})
 mapDoubleLeader("n", "f", ":HopChar1AC<CR>", {})
 mapDoubleLeader("n", "F", ":HopChar1BC<CR>", {})
 mapDoubleLeader("n", "W", ":HopWord<CR>", {})
-
--- Harpoon
-map("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>")
-map("n", "<leader>hl", ":lua require('harpoon.ui').toggle_quick_menu()<CR>")
