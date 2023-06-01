41c205936106b8a8b2339bd2527a3ca108576b78
Gergert Ilya <gergert.ilya@mail.ru>
2023-06-01 16:41:09 +0300

    refactor: change some settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,8 +1,7 @@
-local cmd = vim.cmd -- execute Vim commands
-local exec = vim.api.nvim_exec -- execute Vimscript
-local g = vim.g -- global variables
+local cmd = vim.cmd
+local exec = vim.api.nvim_exec
+local g = vim.g
 local opt = vim.opt
--- Направление перевода с русского на английский
 g.translate_source = "ru"
 g.translate_target = "en"
 opt.fillchars = "eob: "
@@ -19,38 +18,17 @@ opt.so = 10
 opt.undofile = true -- Возможность отката назад
 opt.mouse = "a"
 g.mapleader = " "
--- COLOR
------------------------------------------------------------
-
--- Табы и отступы
------------------------------------------------------------
-
 cmd([[
 filetype indent plugin on
 syntax enable
 ]])
 
-opt.shiftwidth = 2 -- shift 4 spaces when tab
+opt.shiftwidth = 2
+opt.tabstop = 2
 opt.smartindent = true -- autoindent new lines
 -- don't auto commenting new lines
-cmd([[au BufEnter * set fo-=c fo-=r fo-=o]])
--- remove line lenght marker for selected filetypes
-cmd([[autocmd FileType text,markdown,html,xhtml,javascript setlocal cc=0]])
--- 2 spaces for selected filetypes
-cmd([[
-autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml,htmljinja setlocal shiftwidth=2 tabstop=2
-]])
--- С этой строкой отлично форматирует html файл, который содержит jinja2
-cmd([[ autocmd BufNewFile,BufRead *.html set filetype=htmldjango ]])
------------------------------------------------------------
--- Полезные фишки
------------------------------------------------------------
--- Запоминает где nvim последний раз редактировал файл
-cmd([[
-autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-map('n', '<Space>', '<PageDown> zz', default_opts)
-]])
--- Подсвечивает на доли секунды скопированную часть текста
+-- cmd([[au BufEnter * set fo-=c fo-=r fo-=o]])
+-- Copy highlight
 exec(
 	[[
 augroup YankHighlight
