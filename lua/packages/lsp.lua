1d1ac725ef022e91a60a90beb236ceebc74f0621
Gergert Ilya <gergert.ilya@mail.ru>
2023-06-12 17:52:25 +0300

    feat: luasnip lazy_load, lspkink symbol_text cmp

--- lua/packages/lsp.lua
+++ lua/packages/lsp.lua
@@ -9,8 +9,8 @@ luasnip.setup({
 	delete_check_events = "TextChanged,InsertLeave",
 })
 
-require("luasnip.loaders.from_vscode").load()
-luasnip.filetype_extend("typescript", { "typescriptreact", "javascript", "javascriptreact" })
+require("luasnip.loaders.from_vscode").lazy_load()
+luasnip.filetype_extend("typescript", { "typescriptreact", "javascript" })
 luasnip.filetype_extend("javascript", { "typescript", "javascriptreact" })
 
 cmp.setup({
@@ -55,7 +55,7 @@ cmp.setup({
 	}),
 	formatting = {
 		format = lspkind.cmp_format({
-			mode = "symbol", -- show only symbol annotations
+			mode = "symbol_text", -- show only symbol annotations
 			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 
