192387044b13e79b40a0edd9897a0b87b3b4547f
Gergert Ilya <gergert.ilya@mail.ru>
2023-06-23 23:09:02 +0300

    feat: move gui settings to gui.lua and some nvide adds

--- init.lua
+++ init.lua
@@ -1,8 +1,4 @@
 require("plugins")
 require("settings")
 require("keymaps")
-
--- vim.g.neovide_fullscreen = true
-vim.g.neovide_cursor_animate_in_insert_mode = true
-vim.g.neovide_cursor_antialiasing = true
-vim.o.guifont = "JetBrainsMonoNL Nerd Font Mono:h13.6"
+require("gui")
---
+++ lua/gui.lua
@@ -0,0 +1,28 @@
+vim.o.guifont = "JetBrainsMono Nerd Font:h13.6"
+vim.opt.title = true
+vim.opt.titlestring = vim.loop.cwd()
+
+if vim.g.neovide then
+	vim.g.neovide_cursor_animate_in_insert_mode = false
+	vim.g.neovide_cursor_antialiasing = true
+	vim.g.neovide_fullscreen = false
+	-- vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
+	vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+
+	vim.g.neovide_scale_factor = 1.0
+	local change_scale_factor = function(delta)
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+	end
+	local diff = 1.1
+	vim.keymap.set("n", "<A-=>", function()
+		change_scale_factor(diff)
+	end)
+	vim.keymap.set("n", "<A-->", function()
+		change_scale_factor(1 / diff)
+	end)
+end
