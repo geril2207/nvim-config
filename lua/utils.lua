9de3d32760461069e6b00459e888700a317c2e16
Gergert Ilya <gergert.ilya@mail.ru>
2023-06-25 22:23:22 +0300

    feat: add gui goneovim

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,9 +1,14 @@
 local M = {}
 
-M.is_nvim_qt = vim.fn.has("gui_running") == 1 and not vim.g.neovide and not vim.g.fvim_loaded and not vim.g.nvy
 M.is_fvim = vim.g.fvim_loaded
+M.is_goneovim = vim.g.goneovim == 1
 M.is_nvy = vim.g.nvy
 M.is_neovide = vim.g.neovide
 M.is_gui = vim.fn.has("gui_running") == 1
+M.is_nvim_qt = vim.fn.has("gui_running") == 1
+	and not vim.g.neovide
+	and not vim.g.fvim_loaded
+	and not vim.g.nvy
+	and not M.is_goneovim
 
 return M
