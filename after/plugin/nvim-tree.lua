b29aea0335fa28f66fbe2e812b4ff183d74f7ecf
Gergert Ilya <gergert.ilya@mail.ru>
2023-06-27 15:58:53 +0300

    feat: add "l" keymap to open file nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -12,8 +12,19 @@ require("nvim-web-devicons").setup({
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
+local function on_attach(bufnr)
+	local api = require("nvim-tree.api")
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+	api.config.mappings.default_on_attach(bufnr)
+
+	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+end
+
 -- empty setup using defaults
 require("nvim-tree").setup({
+	on_attach = on_attach,
 	hijack_netrw = true,
 	disable_netrw = true,
 	update_focused_file = {
