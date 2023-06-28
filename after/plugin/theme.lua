11c6c77a9f1748252c58a11b41fa3753f830718d
Gergert Ilya <gergert.ilya@mail.ru>
2023-06-28 10:43:55 +0300

    feat: lualine filename relative path

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -95,7 +95,13 @@ require("lualine").setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
+		lualine_c = {
+			{
+				"filename",
+				file_status = false, -- displays file status (readonly status, modified status)
+				path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+			},
+		},
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
 
