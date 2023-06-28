cd5ab85a5f03f71a48140eb78934266520973a87
Gergert Ilya <gergert.ilya@mail.ru>
2023-06-28 12:48:23 +0300

    feat: add n,N maps with centering(zz)

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -54,6 +54,9 @@ local function execute_scroll(direction)
 	vim.api.nvim_command("normal! zz")
 end
 
+map("n", "n", "nzz")
+map("n", "N", "Nzz")
+
 vim.keymap.set("n", "<C-d>", function()
 	execute_scroll("j")
 end)
