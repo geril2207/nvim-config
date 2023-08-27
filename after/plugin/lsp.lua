6dcac9cf2655353559fdc95c5cc36255b1f52dd7
Gergert Ilya <gergert.ilya@mail.ru>
2023-08-27 19:21:07 +0300

    feat(cmp): move buffer cmp to only buffer

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -74,8 +74,7 @@ cmp.setup({
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 		{ name = "path" },
-		{ name = "buffer" },
-	}),
+	}, { name = "buffer" }),
 })
 
 require("nvim-autopairs").setup({
