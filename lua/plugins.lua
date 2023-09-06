75e7bb40221e69896c85352f4c914e64ed891cd9
Gergert Ilya <gergert.ilya@mail.ru>
2023-09-06 15:57:36 +0300

    feat: gitsigns line_blame delay 200

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -84,7 +84,11 @@ require("lazy").setup({
 		"lewis6991/gitsigns.nvim",
 		-- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
 		config = function()
-			require("gitsigns").setup()
+			require("gitsigns").setup({
+				current_line_blame_opts = {
+					delay = 200,
+				},
+			})
 		end,
 	},
 	{
