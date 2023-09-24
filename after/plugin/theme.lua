3c16a2ad2006de23d823b79ea03c0eafeb168ea9
Gergert Ilya <gergert.ilya@mail.ru>
2023-09-24 10:56:05 +0300

    feat: lualine relative path with folders

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -93,21 +93,21 @@ require("lualine").setup({
 	-- 		winbar = 1000,
 	-- 	},
 	-- },
-	-- sections = {
-	-- 	lualine_a = { "mode" },
-	-- 	lualine_b = { "branch", "diff", "diagnostics" },
-	-- 	lualine_c = {
-	-- 		{
-	-- 			"filename",
-	-- 			file_status = false, -- displays file status (readonly status, modified status)
-	-- 			path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
-	-- 		},
-	-- 	},
-	-- 	lualine_x = { "encoding", "fileformat", "filetype" },
-	-- 	lualine_y = { "progress" },
-	--
-	-- 	lualine_z = { "location" },
-	-- },
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = {
+			{
+				"filename",
+				file_status = false, -- displays file status (readonly status, modified status)
+				path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+			},
+		},
+		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_y = { "progress" },
+
+		lualine_z = { "location" },
+	},
 	-- inactive_sections = {
 	-- 	lualine_a = {},
 	-- 	lualine_b = {},
