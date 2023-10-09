5450f555687c99b60237535cc368588ead72321f
Gergert Ilya <gergert.ilya@mail.ru>
2023-10-09 11:39:30 +0300

    feat: add ibl

---
+++ lua/plugins/ibl.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		event = "VeryLazy",
+		opts = {
+			indent = { char = "▏" },
+			-- indent = { char = "│" },
+			scope = { enabled = false },
+		},
+	},
+}
