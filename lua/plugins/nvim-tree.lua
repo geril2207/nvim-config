35ea55e12589f3c0ddb58004c8f75eef25000c64
Gergert Ilya <gergert.ilya@mail.ru>
2023-10-16 14:12:49 +0300

    chore(lazy): update plugins

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -23,7 +23,6 @@ return {
 		"nvim-tree/nvim-tree.lua",
 		priority = 100,
 		version = "*",
-		commit = "d8e495b2354058276cad6dd32e3efdd1d02f4da6",
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 		},
