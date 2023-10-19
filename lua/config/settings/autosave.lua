65e297715a7622e06b7c5e4d0a5b2f50c7637ba6
Gergert Ilya <gergert.ilya@mail.ru>
2023-10-19 10:34:01 +0300

    feat: custom autosave

---
+++ lua/config/settings/autosave.lua
@@ -0,0 +1,23 @@
+local group = vim.api.nvim_create_augroup("AutoSave", { clear = true })
+
+local filetypes_to_ignore = { "harpoon" }
+
+vim.api.nvim_create_autocmd({ "BufLeave", "TextChanged", "InsertLeave" }, {
+	group = group,
+	desc = "AutoSave",
+	callback = function(opts)
+		local modifiable = vim.bo.modifiable
+		local filetype = vim.bo.filetype
+		local file = opts.file
+
+		if modifiable and file ~= "" then
+			for _, value in ipairs(filetypes_to_ignore) do
+				if value == filetype then
+					return
+				end
+			end
+
+			vim.cmd("w " .. file)
+		end
+	end,
+})
--- lua/config/settings/init.lua
+++ lua/config/settings/init.lua
@@ -1,2 +1,3 @@
 require("config.settings.common")
 require("config.settings.filetype")
+require("config.settings.autosave")
