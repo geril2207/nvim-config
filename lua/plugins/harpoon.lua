ca4d76d5515d4b289bd64b342272d7d5e5393167
Gergert Ilya <gergert.ilya@mail.ru>
2023-10-20 19:42:18 +0300

    feat: harpoon explicit tabline = false

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,7 +3,11 @@ return {
 		"geril2207/harpoon",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {
+			tabline = false,
 			save_on_toggle = false,
+			global_settings = {
+				tabline = false,
+			},
 		},
 	},
 }
