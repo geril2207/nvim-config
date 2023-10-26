4d95b9007afa95d57c1a62fcd8c61f2d1f203c93
Gergert Ilya <gergert.ilya@mail.ru>
2023-10-26 12:47:22 +0300

    refactor: common settings

--- lua/config/settings/common.lua
+++ lua/config/settings/common.lua
@@ -2,11 +2,14 @@ local cmd = vim.cmd
 local exec = vim.api.nvim_exec2
 local g = vim.g
 local opt = vim.opt
+local api = vim.api
+
+g.mapleader = " "
 g.translate_source = "ru"
 g.translate_target = "en"
+
 opt.fillchars = "eob: "
 opt.swapfile = false
-
 opt.signcolumn = "yes"
 opt.ignorecase = true
 opt.cmdheight = 1
@@ -17,7 +20,6 @@ opt.relativenumber = true
 opt.so = 10
 opt.undofile = true
 opt.mouse = "a"
-g.mapleader = " "
 opt.tabstop = 2
 opt.softtabstop = 2
 opt.shiftwidth = 2
@@ -25,35 +27,14 @@ opt.expandtab = true
 opt.smartindent = true
 opt.timeoutlen = 500
 opt.clipboard = "unnamedplus"
-
--- opt.guicursor = ""
+opt.updatetime = 250
+opt.cursorline = true
+opt.cursorlineopt = "number"
 opt.showmode = false
 
--- don't auto commenting new lines
--- cmd([[au BufEnter * set fo-=c fo-=r fo-=o]])
--- Copy highlight
-exec(
-	[[
-augroup YankHighlight
-autocmd!
-autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=100}
-augroup end
-]],
-	{ output = false }
-)
-cmd([[
-filetype indent plugin on
-syntax enable
-]])
-
-exec([[autocmd FileType * set formatoptions-=cro]], { output = false })
--- SET HTML AS HTML NOT DJANGO
-cmd([[au BufNewFile,BufRead *.html set filetype=html]])
-
--- Lsp diagnostic auto
--- You will likely want to reduce updatetime which affects CursorHold
--- note: this setting is global and should be set only once
-vim.o.updatetime = 250
-
-opt.cursorline = true -- Highlight the current line
-opt.cursorlineopt = "number"
+api.nvim_create_autocmd("TextYankPost", {
+	group = api.nvim_create_augroup("YankHighlight", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 100 })
+	end,
+})
