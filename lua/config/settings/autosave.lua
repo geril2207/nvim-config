eddaf3056ccb050ac9fa578e88a503a64fb3f301
Gergert Ilya <gergert.ilya@mail.ru>
2023-11-03 14:16:36 +0300

    feat: update autosave

--- lua/config/settings/autosave.lua
+++ lua/config/settings/autosave.lua
@@ -4,29 +4,37 @@ local group = api.nvim_create_augroup("AutoSave", { clear = true })
 
 local filetypes_to_ignore = { "harpoon" }
 
-api.nvim_create_autocmd({ "BufLeave", "TextChanged", "InsertLeave" }, {
+--- @param buf number
+--- @return boolean
+local function get_is_need_write_buf(buf)
+	local modifiable = vim.bo[buf].modifiable
+	local readonly = vim.bo[buf].readonly
+	local filetype = vim.bo[buf].filetype
+	local modified = vim.bo[buf].modified
+
+	if not modified or not modifiable or readonly or file == "" then
+		return false
+	end
+
+	for _, value in ipairs(filetypes_to_ignore) do
+		if value == filetype then
+			return false
+		end
+	end
+
+	return true
+end
+
+api.nvim_create_autocmd({ "TextChanged", "InsertLeave", "BufLeave" }, {
 	group = group,
 	desc = "AutoSave",
 	callback = function(opts)
-		local modifiable = vim.bo.modifiable
-		local readonly = vim.bo.readonly
-		local filetype = vim.bo.filetype
-		local file = opts.file
+		-- local can_write = get_is_need_write_buf(opts.buf)
+		--
+		-- if not can_write then
+		-- 	return
+		-- end
 
-		local is_modified = api.nvim_get_option_value("modified", { buf = 0 })
-
-		if not is_modified then
-			return
-		end
-
-		if modifiable and not readonly and file ~= "" then
-			for _, value in ipairs(filetypes_to_ignore) do
-				if value == filetype then
-					return
-				end
-			end
-
-			vim.cmd("silent! write")
-		end
+		vim.cmd("silent! wa")
 	end,
 })
