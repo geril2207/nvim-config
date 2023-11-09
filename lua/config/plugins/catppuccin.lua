6066992a1f9f2675c2b9a82a96caf048c91fdc5a
Gergert Ilya <gergert.ilya@mail.ru>
2023-11-09 17:22:11 +0300

    SQUASH me DADDY

--- lua/config/plugins/catppuccin.lua
+++ lua/config/plugins/catppuccin.lua
@@ -17,7 +17,7 @@ require("catppuccin").setup({
 				DiagnosticVirtualTextInfo = { fg = colors.peach, bg = colors.none },
 				DiagnosticVirtualTextError = { fg = colors.red, bg = colors.none },
 				-- CursorLineNr = { fg = colors.text, bg = colors.none },
-				NvimTreeCursorLine = { fg = colors.peach, bg = colors.none },
+				NvimTreeCursorLine = { fg = colors.peach, bg = transparent and colors.none or colors.surface0 },
 				NvimTreeCursorLineNr = { fg = colors.text, bg = colors.none },
 				-- NvimTreeIndentMarker = { fg = colors.text, force = true },
 				TelescopeSelection = {
