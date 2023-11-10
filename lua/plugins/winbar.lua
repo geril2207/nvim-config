45682b475da3dd1bcf352f8a41202c7fff3d3416
geril2207 <gergert.ilya@mail.ru>
2023-11-10 11:55:01 +0300

    feat: update winbar

--- lua/plugins/winbar.lua
+++ lua/plugins/winbar.lua
@@ -2,13 +2,25 @@ return {
 	{
 		"geril2207/winbar.nvim",
 		priority = 1,
-		branch = "feat/folder-icon",
+		branch = "main",
+		dev = false,
 		config = function()
 			local colors = require("catppuccin.palettes").get_palette("mocha")
 			require("winbar").setup({
 				enabled = true,
+				change_events = { "DirChanged", "BufEnter", "BufFilePost", "BufWritePost" },
 				show_file_path = true,
 				show_symbols = false,
+				left_spacing = function()
+					local textoff = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].textoff
+					local value = ""
+
+					for _ = 1, textoff - 2, 1 do
+						value = value .. " "
+					end
+
+					return value
+				end,
 
 				colors = {
 					path = colors.overlay0,
@@ -37,6 +49,7 @@ return {
 					"toggleterm",
 					"qf",
 					"harpoon",
+					"TelescopePrompt",
 				},
 			})
 		end,
