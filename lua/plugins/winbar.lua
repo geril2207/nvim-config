1c7f9f38759d95418cb58fd41537b8f09d37b807
geril2207 <gergert.ilya@mail.ru>
2023-11-10 14:31:32 +0300

    feat: switch to simple-winbar.nvim

--- lua/plugins/winbar.lua
+++ lua/plugins/winbar.lua
@@ -1,40 +1,10 @@
 return {
 	{
-		"geril2207/winbar.nvim",
+		"geril2207/simple-winbar.nvim",
 		priority = 1,
-		branch = "main",
-		dev = false,
 		config = function()
-			local colors = require("catppuccin.palettes").get_palette("mocha")
-			require("winbar").setup({
-				enabled = true,
-				change_events = { "DirChanged", "BufEnter", "BufFilePost", "BufWritePost" },
-				show_file_path = true,
-				show_symbols = false,
-				left_spacing = function()
-					local textoff = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].textoff
-					local value = ""
-
-					for _ = 1, textoff - 2, 1 do
-						value = value .. " "
-					end
-
-					return value
-				end,
-
-				colors = {
-					path = colors.overlay0,
-					file_name = colors.flamingo,
-				},
-
-				icons = {
-					file_icon_default = "",
-					seperator = "›",
-					editor_state = "●",
-					lock_icon = "",
-				},
-
-				exclude_filetype = {
+			require("simple-winbar").setup({
+				exclude_filetypes = {
 					"help",
 					"startify",
 					"dashboard",
@@ -51,7 +21,74 @@ return {
 					"harpoon",
 					"TelescopePrompt",
 				},
+
+				left_spacing = function()
+					local textoff = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].textoff
+					local value = ""
+
+					for _ = 1, textoff - 2, 1 do
+						value = value .. " "
+					end
+
+					return value
+				end,
 			})
 		end,
 	},
+	-- {
+	-- 	"geril2207/winbar.nvim",
+	-- 	priority = 1,
+	-- 	branch = "main",
+	-- 	enabled = false,
+	-- 	dev = false,
+	-- 	config = function()
+	-- 		local colors = require("catppuccin.palettes").get_palette("mocha")
+	-- 		require("winbar").setup({
+	-- 			enabled = true,
+	-- 			change_events = { "DirChanged", "BufEnter", "BufFilePost", "BufWritePost" },
+	-- 			show_file_path = true,
+	-- 			show_symbols = false,
+	-- 			left_spacing = function()
+	-- 				local textoff = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].textoff
+	-- 				local value = ""
+	--
+	-- 				for _ = 1, textoff - 2, 1 do
+	-- 					value = value .. " "
+	-- 				end
+	--
+	-- 				return value
+	-- 			end,
+	--
+	-- 			colors = {
+	-- 				path = colors.overlay0,
+	-- 				file_name = colors.flamingo,
+	-- 			},
+	--
+	-- 			icons = {
+	-- 				file_icon_default = "",
+	-- 				seperator = "›",
+	-- 				editor_state = "●",
+	-- 				lock_icon = "",
+	-- 			},
+	--
+	-- 			exclude_filetype = {
+	-- 				"help",
+	-- 				"startify",
+	-- 				"dashboard",
+	-- 				"packer",
+	-- 				"neogitstatus",
+	-- 				"NvimTree",
+	-- 				"Trouble",
+	-- 				"alpha",
+	-- 				"lir",
+	-- 				"Outline",
+	-- 				"spectre_panel",
+	-- 				"toggleterm",
+	-- 				"qf",
+	-- 				"harpoon",
+	-- 				"TelescopePrompt",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 }
