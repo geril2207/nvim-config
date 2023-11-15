c4a270bd2dc1a547be176850f8193ad1f45cb12b
geril2207 <gergert.ilya@mail.ru>
2023-11-15 10:12:30 +0300

    feat: add split open harpoon menu

--- after/ftplugin/harpoon.lua
+++ after/ftplugin/harpoon.lua
@@ -1,1 +1,15 @@
 vim.wo.winbar = nil
+
+-- vertical split alt+v
+vim.keymap.set("n", "<M-v>", function()
+	local curline = vim.api.nvim_get_current_line()
+	local working_directory = vim.fn.getcwd() .. "/"
+	vim.cmd("vsp " .. working_directory .. curline)
+end, { buffer = true, noremap = true, silent = true })
+
+-- horizontal split alt+h
+vim.keymap.set("n", "<M-h>", function()
+	local curline = vim.api.nvim_get_current_line()
+	local working_directory = vim.fn.getcwd() .. "/"
+	vim.cmd("sp " .. working_directory .. curline)
+end, { buffer = true, noremap = true, silent = true })
